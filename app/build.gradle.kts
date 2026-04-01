731930a65630f94f11f5033a5f9049e5c969b69d
VillainAlexandre <villainalexandre89@gmail.com>
2026-04-01 09:05:09 +0200

    correction dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation(libs.okhttp)
     implementation(libs.coil.compose)
     implementation(libs.retrofit)
-    implementation(libs.osmdroid)
+    implementation(libs.osmdroid.android)
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
 }
